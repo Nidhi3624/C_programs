6a6ae38f4df10613177e2e35b01a5eb2e7d9f837
Nidhi3624 <122336909+Nidhi3624@users.noreply.github.com>
2023-08-18 22:47:04 +0530

    Add files via upload

---
+++ a2022dec2c.c
@@ -0,0 +1,15 @@
+//wap to find length of the string
+
+#include<stdio.h>
+#include<string.h>
+
+void main()
+{
+    char str[10];
+    int i, j;
+    
+    printf("enter a string:");
+    gets(str);
+
+    printf("%s", str);
+}
